5bd540c1a52f39ee71e4a6367a4e26e8940a57a2
Insang <62378688+insang1102@users.noreply.github.com>
2020-05-23 22:46:58 +0100

    Update RPS.js

--- RockPaperScissor/RPS.js
+++ RockPaperScissor/RPS.js
@@ -1,18 +1,34 @@
-var left = 0;
+var computerChoice = 0; // the longer and more specific names are, the better
+var RPS = {
+  // This is 'dictionary',, 1:N is possible as well like Korean English property,,
+  rock: "0",
+  scissor: "-142px",
+  paper: "-284px",
+};
+
+var RPS2 = {
+  "0": "rock",
+  "-142px": "scissor",
+  "-284px": "paper",
+};
+
 setInterval(function () {
-  if (left === 0) {
-    left = "-142px";
-  } else if (left === "-142px") {
-    left = "-284px";
+  if (computerChoice === RPS.rock) {
+    computerChoice = RPS.scissor;
+  } else if (computerChoice === RPS.scissor) {
+    computerChoice = RPS.paper;
   } else {
-    left = 0;
+    computerChoice = RPS.rock;
   }
   document.querySelector("#computer").style.background =
-    "url(https://en.pimg.jp/023/182/267/1/23182267.jpg)" + left + " 0";
+    "url(https://en.pimg.jp/023/182/267/1/23182267.jpg)" +
+    computerChoice +
+    " 0";
 }, 100);
 
 document.querySelectorAll(".btn").forEach(function (btn) {
   btn.addEventListener("click", function () {
-    console.log(this.textContent);
+    var myChoice = this.textContent;
+    console.log(myChoice, RPS2[computerChoice]);
   });
 });
