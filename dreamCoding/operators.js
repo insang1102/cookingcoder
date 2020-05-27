f8307d9f47f933a14d4f85eda39c725055f743b2
Insang <62378688+insang1102@users.noreply.github.com>
2020-05-27 23:17:57 +0100

    updated
    up-to-date

--- dreamCoding/operators.js
+++ dreamCoding/operators.js
@@ -17,15 +17,15 @@ let counter = 2;
 const preIncrement = ++counter;
 // counter = counter + 1;
 // preIncrement = counter;
-console.log("preIncrement: ${preIncrement}, counter: ${counter}");
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`);
 const postIncrement = counter++;
 // postIncrement = counter;
 // counter = counter + 1;
-console.log("postIncrement: ${postIncrement}, counter: ${counter}");
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`);
 const preDecrement = --counter;
-console.log("preDecrement: ${preDecrement}, counter: ${counter}");
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`);
 const postDecrement = counter--;
-console.log("postDecrement: ${postDecrement), counter: ${counter}");
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`);
 
 // 4. Assignment operators
 let x = 3;
@@ -48,6 +48,15 @@ const value2 = 4 < 2; // false
 // || (or), finds the first truthy value
 console.log(`or: ${value1 || value2 || check()}`); // the heavier operator is, the latter you'd better input!!
 
+// && (and), finds the first falsy value
+console.log(`and: ${value1 && value2 && check()}`); // same here!!
+
+// often used to compress long if-statement
+// nullableObject && nullableObject.something
+if (nullableObject != null) {
+  nullableObject.something;
+}
+
 function check() {
   for (let i = 0; i < 10; i++) {
     // wasting time
@@ -55,3 +64,130 @@ function check() {
   }
   return true;
 }
+
+// ! (not)
+console.log(!value1);
+
+// 7. Equality
+const stringFive = "5";
+const numberFive = 5;
+
+// == loose equality, with type conversion
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+
+// === string equality, no type conversion
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+
+// object equality by reference
+const insang1 = { name: "insang" };
+const insang2 = { name: "insang" };
+const insang3 = insang1;
+console.log(insang1 == insang2); // insang1 -> ref1 -> name, age ,, like this
+console.log(insang1 === insang2);
+console.llog(insang1 === insang3);
+
+// eqaulity - puzzler
+console.log(0 == false); // true =>>> 0, null, undefined, empty space are recognised as false
+console.log(0 === false); // false =>> not boolean type!
+console.log("" == false); // true
+console.log("" === false); // false
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = 'insang';
+if (name === 'insang') {
+    console.log('Welcome, Insang!');
+} else if (name === 'coder') {
+    console.log('You are a remarkable coder');
+} else {
+    console.log('unknown');
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(name === 'insang' ? 'yes' : 'no'); // nesting is not recommended because of readability
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = 'IE';
+switch (browser) {
+    case 'IE':
+        console.log('go away!');
+        break;
+    case 'Chrome':
+    case 'Firefox':
+        console.log('love you!');
+        break;
+    default;
+        console.log('same all');
+        break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is exectued
+let i = 3;
+while (i > 0) {
+    console.log(`while: ${i}`);
+    i--;
+}
+
+// do while loop, body code is executed first,
+// then check the condition
+do {
+    console.log(`do while: ${i}`);
+    i--;
+} while (i > 0);
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+    console.log(`for: ${i}`);
+}
+
+for (let i =3; i > 0; i = i - 2) {
+    // inline variable declaration
+    console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log(`i: ${i}, j:${j}`);
+    }
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers
+// (use continue)
+for (let i = 0; i < 11; i++) {
+    if (i % 2 !== 0) {
+        continue;
+    }
+    console.log(`q1. ${i}`);
+}
+// Q2. iterate from 0 to 10 and print numbers until reaching 8
+// (use break)
+for (let i = 0; i < 11; i++) {
+    if (i < 8) {
+        break;
+    }
+    console.log(`q2. ${i}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
